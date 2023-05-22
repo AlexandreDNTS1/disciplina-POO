a5d0998d25c68a424928c73b210dfe4fa90c389a
Francimar Alexandre de Oliveira Dantas <alexandreoliveiradt@gmail.com>
2023-05-22 12:01:14 -0300

    receita 8 q1 - q5

---
+++ receita 8/receita8_q4/android/app/src/main/kotlin/com/example/receita8_q4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.receita8_q4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ receita 8/receita8_q4/lib/main.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+enum TableStatus { idle, loading, ready, error }
+
+class DataService {
+  final ValueNotifier<Map<String, dynamic>> tableStateNotifier = ValueNotifier({
+    'status': TableStatus.idle,
+    'dataObjects': [],
+    'columnNames': [],
+    'propertyNames': []
+  });
+
+  void carregar(index) {
+    final funcoes = [carregarCafes, carregarCervejas, carregarNacoes];
+
+    tableStateNotifier.value = {
+      'status': TableStatus.loading,
+      'dataObjects': [],
+      'columnNames': [],
+      'propertyNames': [],
+    };
+
+    funcoes[index]();
+  }
+
+  void carregarCafes() {
+    var coffeUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/coffee/random_coffee',
+        queryParameters: {'size': '5'});
+
+    http.read(coffeUri).then((jsonString) {
+      var coffeJson = jsonDecode(jsonString);
+
+      tableStateNotifier.value = {
+        'status': TableStatus.ready,
+        'dataObjects': coffeJson,
+        'propertyNames': ["blend_name", "origin", "variety", "notes"],
+        'columnNames': ["Blend", "Origem", "Variedade", "Notas"]
+      };
+    });
+  }
+
+  Future<void> carregarNacoes() async {
+    var nationUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/nation/random_nation',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(nationUri);
+
+    var nationJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = {
+      'status': TableStatus.ready,
+      'dataObjects': nationJson,
+      'propertyNames': ["nationality", "language", "capital", "flag"],
+      'columnNames': ["Nacionalidade", "Lingua", "Capital", "Bandeira"]
+    };
+  }
+
+
+
+  void carregarCervejas() {
+    var beersUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/beer/random_beer',
+        queryParameters: {'size': '5'});
+
+    http.read(beersUri).then((jsonString) {
+      var beersJson = jsonDecode(jsonString);
+
+      tableStateNotifier.value = {
+        'status': TableStatus.ready,
+        'dataObjects': beersJson,
+        'propertyNames': ["name", "style", "ibu"],
+        'columnNames': ["Nome", "Estilo", "IBU"]
+      };
+    });
+  }
+}
+
+final dataService = DataService();
+
+void main() {
+  MyApp app = MyApp();
+
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dicas"),
+          ),
+          body: ValueListenableBuilder(
+              valueListenable: dataService.tableStateNotifier,
+              builder: (_, value, __) {
+                switch (value['status']) {
+                  case TableStatus.idle:
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('imagens/welcome_image.png', // Caminho da imagem
+                            width: 200,
+                            height: 200),
+                        SizedBox(height: 16),
+               
+                      
+                        SizedBox(height: 16),
+                        Center(
+                          child: Text(
+                            " aperte os botões abaixo",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+
+                  case TableStatus.loading:
+                    return Center(child: CircularProgressIndicator());
+
+                  case TableStatus.ready:
+                    return DataTableWidget(
+                        jsonObjects: value['dataObjects'],
+                        propertyNames: value['propertyNames'],
+                        columnNames: value['columnNames']);
+
+                  case TableStatus.error:
+                    return Text("Lascou");
+                }
+
+                return Text("...");
+              }),
+          bottomNavigationBar:
+              NewNavBar(itemSelectedCallback: dataService.carregar),
+        ));
+  }
+}
+
+class NewNavBar extends HookWidget {
+  final _itemSelectedCallback;
+
+  NewNavBar({itemSelectedCallback})
+      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
+
+  @override
+  Widget build(BuildContext context) {
+    var state = useState(1);
+
+    return BottomNavigationBar(
+        onTap: (index) {
+          state.value = index;
+
+          _itemSelectedCallback(index);
+        },
+        currentIndex: state.value,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Cafés",
+            icon: Icon(Icons.coffee_outlined),
+          ),
+          BottomNavigationBarItem(
+              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+          BottomNavigationBarItem(
+              label: "Nações", icon: Icon(Icons.flag_outlined))
+        ]);
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+
+  final List<String> columnNames;
+
+  final List<String> propertyNames;
+
+  DataTableWidget({
+    this.jsonObjects = const [],
+    this.columnNames = const [],
+    this.propertyNames = const ["name", "style", "ibu"],
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
+  }
+}

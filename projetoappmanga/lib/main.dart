aad033d8223cfd0d948323d076137f28ea24c875
Francimar Alexandre de Oliveira Dantas <alexandreoliveiradt@gmail.com>
2023-05-31 21:08:01 -0300

    reconstruindo o app, exibindo as imagens dos mangas

--- projetoappmanga/lib/main.dart
+++ projetoappmanga/lib/main.dart
@@ -2,264 +2,128 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-enum TableStatus { idle, loading, ready, error }
-
-class DataService {
-  final ValueNotifier<Map<String, dynamic>> tableStateNotifier = ValueNotifier({
-    'status': TableStatus.idle,
-    'dataObjects': [],
-    'columnNames': [],
-    'propertyNames': [],
-    'error': '',
-  });
-
-  void carregar(String mangaName) {
-    tableStateNotifier.value = {
-      'status': TableStatus.loading,
-      'dataObjects': [],
-      'columnNames': [],
-      'propertyNames': [],
-    };
-
-    pesquisarManga(mangaName);
-  }
-
-  void pesquisarManga(String mangaName) {
-    var mangaUri = Uri.https('api.jikan.moe', '/v4/manga', {'q': mangaName});
-
-    http.get(mangaUri).then((response) {
-      if (response.statusCode == 200) {
-        var mangaJson = jsonDecode(response.body);
-
-        List<dynamic> mangaData = mangaJson['data'];
-        List<dynamic> filteredData = mangaData
-            .where((obj) => obj['type'] == 'Manga')
-            .toList(); // Filtrar apenas os dados do tipo "Manga"
-
-        tableStateNotifier.value = {
-          'status': TableStatus.ready,
-          'dataObjects': filteredData,
-          'propertyNames': ["title", "type", "score", "authors"],
-          'columnNames': ["Título", "Tipo", "Pontuação", "Autores/Autor"],
-        };
-      } else {
-        throw Exception('Falha ao pesquisar o mangá');
-      }
-    }).catchError((error) {
-      tableStateNotifier.value = {
-        'status': TableStatus.error,
-        'error': 'Erro ao pesquisar dados do mangá.',
-      };
-    });
-  }
-}
-
-final dataService = DataService();
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final TextEditingController _textFieldController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("BUSCA MANGÁ"),
-        ),
-        body: ValueListenableBuilder(
-          valueListenable: dataService.tableStateNotifier,
-          builder: (_, value, __) {
-            switch (value['status']) {
-              case TableStatus.idle:
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 16),
-                    SizedBox(height: 16),
-                    Center(
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'imagens/logo.png',
-                          ),
-                          Text(
-                            "Seja bem-vindo ao app de busca de mangá",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.black,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                          TextField(
-                            controller: _textFieldController,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Digite o nome do mangá',
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          ElevatedButton(
-                            onPressed: () {
-                              String mangaName = _textFieldController.text;
-                              if (mangaName.isNotEmpty) {
-                                dataService.carregar(mangaName);
-                              }
-                            },
-                            child: Text('Buscar'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              case TableStatus.error:
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Ocorreu um erro ao carregar os dados.",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.red,
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    Text(
-                      value['error'],
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ],
-                );
-              case TableStatus.loading:
-                return Center(child: CircularProgressIndicator());
-              case TableStatus.ready:
-                final dataObjects = value['dataObjects'];
-                if (dataObjects.isNotEmpty) {
-                  return SingleChildScrollView(
-                    child: DataTableWidget(
-                      jsonObjects: dataObjects,
-                      columnNames: value['columnNames'],
-                      propertyNames: value['propertyNames'],
-                    ),
-                  );
-                } else {
-                  return SobreWidget();
-                }
-              default:
-                return Text("...");
-            }
-          },
-        ),
-        floatingActionButton: ['status'] == TableStatus.ready
-            ? FloatingActionButton(
-                onPressed: () {
-                  dataService.tableStateNotifier.value = {
-                    'status': TableStatus.idle,
-                    'dataObjects': [],
-                    'columnNames': [],
-                    'propertyNames': [],
-                    'error': '',
-                  };
-                  _textFieldController.clear();
-                },
-                child: Icon(Icons.arrow_back),
-                backgroundColor: Colors.blue,
-              )
-            : null,
-        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      title: 'API Jikan Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'API Jikan Demo'),
     );
   }
 }
 
-class DataTableWidget extends StatelessWidget {
-  final List<dynamic> jsonObjects;
-  final List<String> columnNames;
-  final List<String> propertyNames;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  DataTableWidget({
-    required this.jsonObjects,
-    required this.columnNames,
-    required this.propertyNames,
-  });
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<dynamic>> mangaList;
+  TextEditingController searchController = TextEditingController();
+  double imageSize = 120.0;
+
+  Future<List<dynamic>> fetchManga(String query) async {
+    final response = await http.get(Uri.parse('https://api.jikan.moe/v4/manga?q=$query'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['data'];
+    } else {
+      throw Exception('Failed to load manga');
+    }
+  }
+
+  void searchManga() {
+    String query = searchController.text;
+    setState(() {
+      mangaList = fetchManga(query);
+    });
+  }
+
+  void toggleImageSize() {
+    setState(() {
+      imageSize = imageSize == 120.0 ? 240.0 : 120.0;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    mangaList = fetchManga('');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: columnNames
-          .map(
-            (name) => DataColumn(
-              label: Text(
-                name,
-                style: TextStyle(fontStyle: FontStyle.italic),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                labelText: 'Pesquisar mangá',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: searchManga,
+                ),
               ),
             ),
-          )
-          .toList(),
-      rows: jsonObjects
-          .map(
-            (obj) => DataRow(
-              cells: propertyNames.map(
-                (propName) {
-                  if (propName == "authors") {
-                    final authorsList = obj[propName] as List<dynamic>?;
-                    final authorNames = authorsList != null
-                        ? authorsList
-                            .map((author) => author['name'].toString())
-                            .join(', ')
-                        : '';
-                    return DataCell(
-                      Text(authorNames),
-                    );
-                  } else {
-                    final propValue = obj[propName];
-                    return DataCell(
-                      propValue != null ? Text(propValue.toString()) : Text('-'),
-                    );
-                  }
-                },
-              ).toList(),
-            ),
-          )
-          .toList(),
-    );
-  }
-}
+          ),
+          Expanded(
+            child: Center(
+              child: FutureBuilder<List<dynamic>>(
+                future: mangaList,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final manga = snapshot.data![index];
+                        final imageUrl = manga['images']['jpg']['image_url'];
 
-class SobreWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(height: 16),
-        SizedBox(height: 16),
-        Center(
-          child: Text(
-            "Seja bem-vindo ao app de busca de mangá\n\nNesse aplicativo, você poderá buscar seu mangá favorito e adicioná-lo aos favoritos.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontStyle: FontStyle.italic,
+                        return Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: GestureDetector(
+                              onTap: toggleImageSize,
+                              child: Image.network(
+                                imageUrl,
+                                fit: BoxFit.contain,
+                                width: imageSize,
+                                height: imageSize,
+                              ),
+                            ),
+                            title: Text(manga['title']),
+                            subtitle: Text('ID: ${manga['mal_id']}'),
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+
+                  return CircularProgressIndicator();
+                },
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
